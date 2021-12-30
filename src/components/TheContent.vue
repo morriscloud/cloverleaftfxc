0c24af706a6fe9be73350b340b769bd02e2330d7
Tony Morris <tony@morriscloud.com>
2021-12-30 15:41:16 -0500

    Small opacity update

--- src/components/TheContent.vue
+++ src/components/TheContent.vue
@@ -1,6 +1,6 @@
 <template>
     <main>
-        <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8 bg-white bg-opacity-75 my-3 dark:bg-black dark:bg-opacity-60 dark:text-white">
+        <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8 bg-white bg-opacity-60 my-3 dark:bg-black dark:bg-opacity-60 dark:text-white">
             <slot></slot>
         </div>
   </main>
