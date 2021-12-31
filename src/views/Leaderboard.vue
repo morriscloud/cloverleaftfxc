292bdc65295057a8c43a812c4f6b739b8f55b8a1
Tony Morris <tony@morriscloud.com>
2021-12-31 15:41:42 -0500

    More leaderboard updates

--- src/views/Leaderboard.vue
+++ src/views/Leaderboard.vue
@@ -1,45 +1,191 @@
 <template>
-  <div class="flex flex-row not-prose">
+  <div class="flex flex-col lg:flex-row not-prose">
     <div class="flex-auto mr-1">
-      <h2 class="dark:text-gray-200 text-center">Boys</h2>
-      <table class="min-w-full divide-y divide-gray-200">
-        <thead class="bg-gray-50 dark:bg-black dark:bg-opacity-50">
+      <h2 class="dark:text-gray-200 text-center text-3xl font-bold mb-2">
+        Boys
+      </h2>
+      <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-800 text-xs font-medium dark:text-white text-gray-500 uppercase tracking-wider">
+        <thead class="bg-gray-50 dark:bg-black dark:bg-opacity-50 text-left">
           <tr>
-            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            <th scope="col" class="px-6 py-3">
               Event
             </th>
-            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            <th scope="col" class="px-6 py-3">
               Athlete
             </th>
-            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            <th scope="col" class="px-6 py-3">
               Performance
             </th>
-            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            <th scope="col" class="px-6 py-3">
               Date
             </th>
           </tr>
         </thead>
+        <tbody class="text-left">
+          <tr>
+            <td colspan="4" class="px-6 py-3 text-center dark:bg-green-800 dark:bg-opacity-50 bg-gray-200 bg-opacity-50">
+              Indoor
+            </td>
+          </tr>
+          <tr>
+            <td class="px-6 py-3">60M</td>
+            <td class="px-6 py-3">Zack Munchick</td>
+            <td class="px-6 py-3">00:07.42</td>
+            <td class="px-6 py-3">2017-02-04</td>
+          </tr>
+          <tr>
+            <td class="px-6 py-3">200M</td>
+            <td class="px-6 py-3">Blake Wilson</td>
+            <td class="px-6 py-3">00:24.01</td>
+            <td class="px-6 py-3">2021-02-20</td>
+          </tr>
+          <tr>
+            <td class="px-6 py-3">400M</td>
+            <td class="px-6 py-3">Andrew Hibler</td>
+            <td class="px-6 py-3">00:55.88</td>
+            <td class="px-6 py-3">2021-02-15</td>
+          </tr>
+          <tr>
+            <td class="px-6 py-3">800M</td>
+            <td class="px-6 py-3">Luke Malena</td>
+            <td class="px-6 py-3">02:12.27</td>
+            <td class="px-6 py-3">2021-01-30</td>
+          </tr>
+          <tr>
+            <td class="px-6 py-3">1600M</td>
+            <td class="px-6 py-3">Brett Gucker</td>
+            <td class="px-6 py-3">05:03.98</td>
+            <td class="px-6 py-3">2021-02-15</td>
+          </tr>
+          <tr>
+            <td class="px-6 py-3">3200M</td>
+            <td class="px-6 py-3">Max Gucker</td>
+            <td class="px-6 py-3">09:38.48</td>
+            <td class="px-6 py-3">2020-02-22</td>
+          </tr>
+          <tr>
+            <td class="px-6 py-3">4x800M Relay</td>
+            <td class="px-6 py-3">
+              Andrew Hibler<br />
+              Brett Gucker<br />
+              Cole Yagersz<br />
+              Nolan Freeland
+            </td>
+            <td class="px-6 py-3">09:38.48</td>
+            <td class="px-6 py-3">2020-02-21</td>
+          </tr>
+          <tr>
+            <td class="px-6 py-3">Long Jump</td>
+            <td class="px-6 py-3">Adam Oren</td>
+            <td class="px-6 py-3">16-09.00</td>
+            <td class="px-6 py-3">2017-02-04</td>
+          </tr>
+          <tr>
+            <td class="px-6 py-3">Shot Put</td>
+            <td class="px-6 py-3">Brian Livingstone</td>
+            <td class="px-6 py-3">45-10.00</td>
+            <td class="px-6 py-3">2012-02-05</td>
+          </tr>
+          <tr>
+            <td colspan="4" class="px-6 py-3 text-center dark:bg-green-800 dark:bg-opacity-50 bg-gray-200 bg-opacity-50">
+              Outdoor
+            </td>
+          </tr>
+        </tbody>
       </table>
     </div>
-    <div class="flex-auto ml-1">
-      <h2 class="dark:text-gray-200 text-center">Girls</h2>
-      <table class="min-w-full divide-y divide-gray-200">
-        <thead class="bg-gray-50">
+    <div class="flex-auto mr-1">
+      <h2 class="dark:text-gray-200 text-center text-3xl font-bold mb-2">
+        Girls
+      </h2>
+      <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-800 text-xs font-medium dark:text-white text-gray-500 uppercase tracking-wider">
+        <thead class="bg-gray-50 dark:bg-black dark:bg-opacity-50 text-left">
           <tr>
-            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            <th scope="col" class="px-6 py-3">
               Event
             </th>
-            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            <th scope="col" class="px-6 py-3">
               Athlete
             </th>
-            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            <th scope="col" class="px-6 py-3">
               Performance
             </th>
-            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            <th scope="col" class="px-6 py-3">
               Date
             </th>
           </tr>
         </thead>
+        <tbody class="text-left">
+          <tr>
+            <td colspan="4" class="px-6 py-3 text-center dark:bg-green-800 dark:bg-opacity-50 bg-gray-200 bg-opacity-50">
+              Indoor
+            </td>
+          </tr>
+          <tr>
+            <td class="px-6 py-3">60M</td>
+            <td class="px-6 py-3">Zack Munchick</td>
+            <td class="px-6 py-3">00:07.42</td>
+            <td class="px-6 py-3">2017-02-04</td>
+          </tr>
+          <tr>
+            <td class="px-6 py-3">200M</td>
+            <td class="px-6 py-3">Blake Wilson</td>
+            <td class="px-6 py-3">00:24.01</td>
+            <td class="px-6 py-3">2021-02-20</td>
+          </tr>
+          <tr>
+            <td class="px-6 py-3">400M</td>
+            <td class="px-6 py-3">Andrew Hibler</td>
+            <td class="px-6 py-3">00:55.88</td>
+            <td class="px-6 py-3">2021-02-15</td>
+          </tr>
+          <tr>
+            <td class="px-6 py-3">800M</td>
+            <td class="px-6 py-3">Luke Malena</td>
+            <td class="px-6 py-3">02:12.27</td>
+            <td class="px-6 py-3">2021-01-30</td>
+          </tr>
+          <tr>
+            <td class="px-6 py-3">1600M</td>
+            <td class="px-6 py-3">Brett Gucker</td>
+            <td class="px-6 py-3">05:03.98</td>
+            <td class="px-6 py-3">2021-02-15</td>
+          </tr>
+          <tr>
+            <td class="px-6 py-3">3200M</td>
+            <td class="px-6 py-3">Max Gucker</td>
+            <td class="px-6 py-3">09:38.48</td>
+            <td class="px-6 py-3">2020-02-22</td>
+          </tr>
+          <tr>
+            <td class="px-6 py-3">4x800M Relay</td>
+            <td class="px-6 py-3">
+              Andrew Hibler<br />
+              Brett Gucker<br />
+              Cole Yagersz<br />
+              Nolan Freeland
+            </td>
+            <td class="px-6 py-3">09:38.48</td>
+            <td class="px-6 py-3">2020-02-21</td>
+          </tr>
+          <tr>
+            <td class="px-6 py-3">Long Jump</td>
+            <td class="px-6 py-3">Adam Oren</td>
+            <td class="px-6 py-3">16-09.00</td>
+            <td class="px-6 py-3">2017-02-04</td>
+          </tr>
+          <tr>
+            <td class="px-6 py-3">Shot Put</td>
+            <td class="px-6 py-3">Brian Livingstone</td>
+            <td class="px-6 py-3">45-10.00</td>
+            <td class="px-6 py-3">2012-02-05</td>
+          </tr>
+          <tr>
+            <td colspan="4" class="px-6 py-3 text-center dark:bg-green-800 dark:bg-opacity-50 bg-gray-200 bg-opacity-50">
+              Outdoor
+            </td>
+          </tr>
+        </tbody>
       </table>
     </div>
   </div>
