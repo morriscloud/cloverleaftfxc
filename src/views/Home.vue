3ffad9acfc4a0f2b69eba82bb28ce6071f77ca39
Tony Morris <tony@morriscloud.com>
2021-12-31 09:30:03 -0500

    More style updates

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,13 @@
 <template>
-  <h2 class="dark:text-gray-200">Welcome!</h2>
-  <p>
-    In total there are <strong class="dark:text-gray-200">xxxx</strong> performances for <strong class="dark:text-gray-200">xxx</strong> from <strong class="dark:text-gray-200">xxx</strong> meets across <strong class="dark:text-gray-200">xx</strong> seasons of Cloverleaf Track and Field.
-  </p>
-  <p>
-    A lot of this website was manually updated, so if something seems wrong or out-of-place, it likely is! Please <a href="mailto:tony@morriscloud.com" class="dark:text-white">email me</a> if you notice anything needs corrected.
-  </p>
+  <div class="prose prose-slate dark:text-white min-w-full">
+    <h2 class="dark:text-gray-200 ">Welcome!</h2>
+    <p>
+      In total there are <strong class="dark:text-gray-200">xxxx</strong> performances for <strong class="dark:text-gray-200">xxx</strong> from <strong class="dark:text-gray-200">xxx</strong> meets across <strong class="dark:text-gray-200">xx</strong> seasons of Cloverleaf Track and Field.
+    </p>
+    <p>
+      A lot of this website was manually updated, so if something seems wrong or out-of-place, it likely is! Please <a href="mailto:tony@morriscloud.com" class="dark:text-white">email me</a> if you notice anything needs corrected.
+    </p>
+  </div>
 </template>
 
 <script setup lang="ts">
--- src/views/Leaderboard.vue
+++ src/views/Leaderboard.vue
@@ -1,5 +1,48 @@
 <template>
-  <table></table>
+  <div class="flex flex-row not-prose">
+    <div class="flex-auto mr-1">
+      <h2 class="dark:text-gray-200 text-center">Boys</h2>
+      <table class="min-w-full divide-y divide-gray-200">
+        <thead class="bg-gray-50 dark:bg-black dark:bg-opacity-50">
+          <tr>
+            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Event
+            </th>
+            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Athlete
+            </th>
+            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Performance
+            </th>
+            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Date
+            </th>
+          </tr>
+        </thead>
+      </table>
+    </div>
+    <div class="flex-auto ml-1">
+      <h2 class="dark:text-gray-200 text-center">Girls</h2>
+      <table class="min-w-full divide-y divide-gray-200">
+        <thead class="bg-gray-50">
+          <tr>
+            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Event
+            </th>
+            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Athlete
+            </th>
+            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Performance
+            </th>
+            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Date
+            </th>
+          </tr>
+        </thead>
+      </table>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
