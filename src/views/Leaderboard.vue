a6c1798f1a1c48ccd70e63629a44d2fc2aad5f33
Tony Morris <tony@morriscloud.com>
2022-04-01 22:09:18 -0400

    Another leaderboard update

--- src/views/Leaderboard.vue
+++ src/views/Leaderboard.vue
@@ -4,16 +4,16 @@
       <h2 class="dark:text-gray-200 text-center text-3xl font-bold mb-2">
         Boys
       </h2>
-      <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-800 text-xs font-medium dark:text-white text-gray-500 tracking-wider">
+      <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-800 text-xs font-medium dark:text-white text-gray-500 tracking-wider table-fixed">
         <thead class="bg-gray-50 dark:bg-black dark:bg-opacity-50 text-left">
           <tr>
-            <th scope="col" class="px-6 py-3">
+            <th scope="col" class="px-6 py-3 w-24">
               Event
             </th>
-            <th scope="col" class="px-6 py-3">
+            <th scope="col" class="px-6 py-3 w-48">
               Athlete
             </th>
-            <th scope="col" class="px-6 py-3">
+            <th scope="col" class="px-6 py-3 w-20">
               Performance
             </th>
             <th scope="col" class="px-6 py-3">
@@ -304,31 +304,31 @@
             <td class="px-6 py-3">2011-03-26</td>
           </tr>
           <tr class="dark:bg-green-900 dark:bg-opacity-25 bg-gray-300 bg-opacity-20">
-            <td class="px-6 py-3">&nbsp;</td>
-            <td class="px-6 py-3">&nbsp;</td>
-            <td class="px-6 py-3">&nbsp;</td>
-            <td class="px-6 py-3">&nbsp;</td>
-          </tr>
-          <tr class="dark:bg-green-900 dark:bg-opacity-10 bg-gray-300 bg-opacity-5">
-            <td class="px-6 py-3">&nbsp;</td>
-            <td class="px-6 py-3">&nbsp;</td>
-            <td class="px-6 py-3">&nbsp;</td>
-            <td class="px-6 py-3">&nbsp;</td>
-          </tr>
-          <tr class="dark:bg-green-900 dark:bg-opacity-25 bg-gray-300 bg-opacity-20">
-            <td class="px-6 py-3">&nbsp;</td>
+            <td class="px-6 py-3">Seated 100M</td>
             <td class="px-6 py-3">&nbsp;</td>
             <td class="px-6 py-3">&nbsp;</td>
             <td class="px-6 py-3">&nbsp;</td>
           </tr>
           <tr class="dark:bg-green-900 dark:bg-opacity-10 bg-gray-300 bg-opacity-5">
-            <td class="px-6 py-3">&nbsp;</td>
+            <td class="px-6 py-3">Seated 200M</td>
             <td class="px-6 py-3">&nbsp;</td>
             <td class="px-6 py-3">&nbsp;</td>
             <td class="px-6 py-3">&nbsp;</td>
           </tr>
           <tr class="dark:bg-green-900 dark:bg-opacity-25 bg-gray-300 bg-opacity-20">
+            <td class="px-6 py-3">Seated 400M</td>
             <td class="px-6 py-3">&nbsp;</td>
+            <td class="px-6 py-3">&nbsp;</td>
+            <td class="px-6 py-3">&nbsp;</td>
+          </tr>
+          <tr class="dark:bg-green-900 dark:bg-opacity-10 bg-gray-300 bg-opacity-5">
+            <td class="px-6 py-3">Seated 800M</td>
+            <td class="px-6 py-3">&nbsp;</td>
+            <td class="px-6 py-3">&nbsp;</td>
+            <td class="px-6 py-3">&nbsp;</td>
+          </tr>
+          <tr class="dark:bg-green-900 dark:bg-opacity-25 bg-gray-300 bg-opacity-20">
+            <td class="px-6 py-3">Seated Shot Put</td>
             <td class="px-6 py-3">&nbsp;</td>
             <td class="px-6 py-3">&nbsp;</td>
             <td class="px-6 py-3">&nbsp;</td>
@@ -375,13 +375,13 @@
             <td class="px-6 py-3">2020-02-22</td>
           </tr>
           <tr class="dark:bg-green-900 dark:bg-opacity-10 bg-gray-300 bg-opacity-5">
-            <td class="px-6 py-3">&nbsp;</td>
+            <td class="px-6 py-3">60M Hurdles</td>
             <td class="px-6 py-3">&nbsp;</td>
             <td class="px-6 py-3">&nbsp;</td>
             <td class="px-6 py-3">&nbsp;</td>
           </tr>
           <tr class="dark:bg-green-900 dark:bg-opacity-25 bg-gray-300 bg-opacity-20">
-            <td class="px-6 py-3">&nbsp;</td>
+            <td class="px-6 py-3">4x400M Relay</td>
             <td class="px-6 py-3">
               &nbsp;<br />
               &nbsp;<br />
@@ -415,13 +415,13 @@
             <td class="px-6 py-3">2012-02-05</td>
           </tr>
           <tr class="dark:bg-green-900 dark:bg-opacity-25 bg-gray-300 bg-opacity-20">
-            <td class="px-6 py-3">&nbsp;</td>
+            <td class="px-6 py-3">Triple Jump</td>
             <td class="px-6 py-3">&nbsp;</td>
             <td class="px-6 py-3">&nbsp;</td>
             <td class="px-6 py-3">&nbsp;</td>
           </tr>
           <tr class="dark:bg-green-900 dark:bg-opacity-10 bg-gray-300 bg-opacity-5">
-            <td class="px-6 py-3">&nbsp;</td>
+            <td class="px-6 py-3">Seated Shot Put</td>
             <td class="px-6 py-3">&nbsp;</td>
             <td class="px-6 py-3">&nbsp;</td>
             <td class="px-6 py-3">&nbsp;</td>
@@ -436,13 +436,13 @@
       <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-800 text-xs font-medium dark:text-white text-gray-500 tracking-wider">
         <thead class="bg-gray-50 dark:bg-black dark:bg-opacity-50 text-left">
           <tr>
-            <th scope="col" class="px-6 py-3">
+            <th scope="col" class="px-6 py-3 w-24">
               Event
             </th>
-            <th scope="col" class="px-6 py-3">
+            <th scope="col" class="px-6 py-3 w-48">
               Athlete
             </th>
-            <th scope="col" class="px-6 py-3">
+            <th scope="col" class="px-6 py-3 w-20">
               Performance
             </th>
             <th scope="col" class="px-6 py-3">
@@ -593,7 +593,7 @@
             <td class="px-6 py-3">2007-05-01</td>
           </tr>
           <tr class="dark:bg-green-900 dark:bg-opacity-10 bg-gray-300 bg-opacity-5">
-            <td class="px-6 py-3"></td>
+            <td class="px-6 py-3">4x300M Shuttle Hurdles</td>
             <td class="px-6 py-3">
               &nbsp;<br />
               &nbsp;<br />
@@ -717,13 +717,13 @@
             <td class="px-6 py-3">2011-03-26</td>
           </tr>
           <tr class="dark:bg-green-900 dark:bg-opacity-25 bg-gray-300 bg-opacity-20">
-            <td class="px-6 py-3">&nbsp;</td>
+            <td class="px-6 py-3">Triple Jump</td>
             <td class="px-6 py-3">&nbsp;</td>
             <td class="px-6 py-3">&nbsp;</td>
             <td class="px-6 py-3">&nbsp;</td>
           </tr>
           <tr class="dark:bg-green-900 dark:bg-opacity-10 bg-gray-300 bg-opacity-5">
-            <td class="px-6 py-3">&nbsp;</td>
+            <td class="px-6 py-3">Triple Jump Relay</td>
             <td class="px-6 py-3">
               &nbsp;<br />
               &nbsp;<br />
